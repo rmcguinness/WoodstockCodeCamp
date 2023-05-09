dbfe1a8c092c52a237c162814efa81cb01d7f490
Heather <80366350+heathjackson@users.noreply.github.com>
2023-05-09 10:09:45 -0400

    About page JSX set up

--- src/About.jsx
+++ src/About.jsx
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import './About.css'
 import AboutProfile from "./AboutProfile";
 
 const About = () => {
