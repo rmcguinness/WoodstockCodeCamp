1226e8b1adc9ca7048f8969f6bde1a30463d1439
Heather <80366350+heathjackson@users.noreply.github.com>
2023-05-09 10:09:45 -0400

    About page JSX set up

--- src/AboutProfile.jsx
+++ src/AboutProfile.jsx
@@ -11,33 +11,39 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+import heatherAvatar from './images-about/IMG_2306.jpeg';
 
 let bioObjs = [{
     id: 0,
     name: 'Heather Jackson',
     email: 'heather.jackson2003@gmail.com',
+    avatar: heatherAvatar,
+    bio: 'I am currently becoming a web-developer through Free Code Camp',
   },
 
   {
     id: 1,
     name: 'Sally Mae',
     email: 'example@gmail.com',
-    
+    avatar: heatherAvatar,
+    bio: '',
   },
 ]
 
 const AboutProfile = () => {
 
   let arrayBioObjs = bioObjs.map(bioObj => 
-    <li key={bioObj.id}>
-      <ul>{bioObj.name}</ul>
-      <ul>{bioObj.email}</ul>
-    </li>
+    <div key={bioObj.id}>
+      <p>{bioObj.name}</p>
+      <email>{bioObj.email}</email>
+      <img src={bioObj.avatar}/>
+      <p>{bioObj.bio}</p>
+    </div>
   )
 
   return(
     <>
-      <ul>{arrayBioObjs}</ul>
+      {arrayBioObjs}
     </>
   );
 }
