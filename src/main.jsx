741132c98bc4f4f4ef16188cf7c48362063ea589
Cody Black <cdblack86@gmail.com>
2023-05-08 22:31:54 -0400

    Added router to main.jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import About from './About.jsx'
+import Help from './Help.jsx'
 import './index.css'
 import { RecoilRoot } from 'recoil';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/About",
+    element: <About />,
+  },
+  {
+    path: "/Help",
+    element: <Help />
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RecoilRoot>
-      <App />
+      <RouterProvider router={router} />
     </RecoilRoot>
   </React.StrictMode>,
 )
