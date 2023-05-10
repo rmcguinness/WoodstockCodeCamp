0213c36b49f8ba250f68a808c79858103623ec37
Ryan McGuinness <ryan@fallingonline.com>
2023-05-10 09:05:15 -0400

    Revert "Added router to main.jsx file"
    This reverts commit 741132c98bc4f4f4ef16188cf7c48362063ea589.

--- src/main.jsx
+++ src/main.jsx
@@ -1,33 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import About from './About.jsx'
-import Help from './Help.jsx'
 import './index.css'
 import { RecoilRoot } from 'recoil';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "/About",
-    element: <About />,
-  },
-  {
-    path: "/Help",
-    element: <Help />
-  }
-]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RecoilRoot>
-      <RouterProvider router={router} />
+      <App />
     </RecoilRoot>
   </React.StrictMode>,
 )
