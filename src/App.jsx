51f8dee57e88c6f2af953191bad54a87d7c4018d
Cody Black <cdblack86@gmail.com>
2023-05-10 11:49:23 -0400

    Corrected Routes and added Layout function for router

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import './App.css'
 import { useRecoilState } from 'recoil'
 import {strState} from './State'
+
+import {Routes, Route, Outlet} from "react-router-dom";
+
 import Header from './Header'
 import Calculator from './Calculator'
 import Help from './Help'
@@ -11,11 +14,18 @@ function App() {
   const [display] = useRecoilState(strState)
   return (
     <>
+
+     <Routes>
+        <Route path="/" element={<Layout />} >
+          <Route index element={<Calculator />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/help" element={<Help />} />
+        </Route>
+      </Routes>
+
       <h1>Calculator Application</h1>
       <div>State value is: {display}</div>
 
-      <Header />
-      <hr/>
       <Calculator />
       <hr/>
       <Help />
@@ -25,6 +35,16 @@ function App() {
       <Footer />
     </>
   )
+}
+function Layout() {
+  return (
+    <div>
+      <Header />
+      <hr />
+      <Outlet />
+      <Footer />
+    </div>
+  );
 }
 
 export default App
