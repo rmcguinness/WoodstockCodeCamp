ab2e18fe778bcfc5861713b6946643c92ae6b55a
Zac Horton <fullstackzac@gmail.com>
2023-05-12 09:45:12 -0400

    Created and styled Help page

--- src/Help.jsx
+++ src/Help.jsx
@@ -11,11 +11,34 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+import './Help.css'
 
 const Help = () => {
   return(
     <>
+      
       <h3>Help</h3>
+      <div className = "help">
+        <p><strong>Note On Calculator Logic:</strong> It should be noted that there are two main 
+        schools of thought on calculator input logic: immediate execution logic and formula logic.
+        Our example utilizes formula logic and observes order of operation precedence, immediate 
+        execution does not. Either is acceptable, but please note that depending on which you choose,
+        your calculator may yield different results than ours for certain equations (see below example). 
+        As long as your math can be verified by another production calculator, please do not consider this a
+        bug.</p>
+        <div className = "flex-container">
+          <div className = "flex-item"><p><strong>EXAMPLE:</strong></p></div>
+          <div className = "flex-item"><p>3 + 5 x 6 - 2 / 4 =</p></div>
+        </div>
+        <div className = "flex-container">
+          <div className = "flex-item"><p><strong>Immediate Execution Logic:</strong></p></div>
+          <div className = "flex-item"><p>11.5</p></div>
+        </div>
+        <div className = "flex-container">
+          <div className = "flex-item"><p><strong>Formula/Expression Logic:</strong></p></div>
+          <div className = "flex-item"><p>32.5</p></div>
+        </div>
+      </div>
     </>
   )
 }
