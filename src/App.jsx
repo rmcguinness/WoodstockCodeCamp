e2afeaaf1f18a4e7759ac053027afae9fced17da
Ryan McGuinness <ryan@fallingonline.com>
2023-05-13 20:40:37 -0400

    Removed additional imports and excess tags

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import './App.css'
-import { useRecoilState } from 'recoil'
-import {Routes, Route, Outlet} from "react-router-dom";
+import { Routes, Route, Outlet } from "react-router-dom";
 import Header from './Header'
 import Calculator from './Calculator'
 import Help from './Help'
@@ -9,16 +8,13 @@ import Footer from './Footer'
 
 function App() {
   return (
-    <>
-
-     <Routes>
-        <Route path="/" element={<Layout />} >
-          <Route index element={<Calculator />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/help" element={<Help />} />
-        </Route>
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<Layout />} >
+        <Route index element={<Calculator />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/help" element={<Help />} />
+      </Route>
+    </Routes>
   )
 }
 function Layout() {
