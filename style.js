2964c60decf692d614acef847d1f36e487d7c58b
JordanD <jjosephd804@gmail.com>
2023-05-21 07:07:44 -0400

    styling updates for the about page. details included in comments

---
+++ style.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Name = styled.h5`
+  font-size: 1rem;
+`;
+
+export const Paragraph = styled.div`
+  margin-top: 10px;
+  height: 100px;
+  width: 190px;
+  line-height: 1;
+  overflow: auto;
+`;
+
+export const Email = styled.div``;
+
+export const Profile = styled.div``;
