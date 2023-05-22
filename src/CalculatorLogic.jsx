b3b7182552b43f8292664e10948b7f8a544ee39b
Josilyn McGuinness <josilyn_butler@yahoo.com>
2023-05-22 17:38:37 -0400

    more logic changes to make things work with new approach adding to equation and fixed calculator logic test cases

--- src/CalculatorLogic.jsx
+++ src/CalculatorLogic.jsx
@@ -1,12 +1,11 @@
 import { evaluate, abs } from 'mathjs';
 
-const isOperator = /[+-\/*]/;
+const isOperator = /\+|-|\*|\//;  //checks for operators
 
 export const handleFunctions = (buttonObj, display, setDisplay, equation, setEquation) => {
   const displayValue = buttonObj.displayValue;
   const equationValue = buttonObj.equationValue;
   const action = buttonObj.action;
-  let lastClickedOperator = false;
 
   //check for and handle operator, AC, or "=" click
   switch (action) {
@@ -14,17 +13,19 @@ export const handleFunctions = (buttonObj, display, setDisplay, equation, setEqu
     case 'divide':
     case 'addition':
     case 'subtract':
-      setEquation(pushOperator(equation, equationValue, display, lastClickedOperator));
+      setEquation(pushOperator(equation, equationValue));
       break;
     case 'clear':
       setEquation('');
       setDisplay('0');
       break;
     case 'pos_neg':
-      setDisplay(changeSign(display, displayValue));
+      let switchSign = changeSign(display, displayValue);
+      setDisplay(switchSign);
+      setEquation(switchSign);
       break;
     case 'equals':
-      setDisplay(calculate(equation, display));
+      setDisplay(calculate(equation));
       setEquation('=');
       break;
   }
@@ -32,7 +33,7 @@ export const handleFunctions = (buttonObj, display, setDisplay, equation, setEqu
 /*If multiple operators are entered sequentially, only use the last entered.
 When an operator is entered, push the displayed numbers to the equation,
 along with the clicked operator. */
-export const pushOperator = (equation, equationValue, display, lastClickedOperator) => {
+export const pushOperator = (equation, equationValue) => {
   if (isOperator.test(equation[equation.length - 1])) {
     return equation.slice(0, -1) + equationValue;
   } else {
@@ -61,7 +62,7 @@ export const calculate = (equation) => {
 //change the sign of the displayed number
 export const changeSign = (display) => {
   if (/\d/.test(display)) {
-    let num = parseFloat(display);  //change string to float
+    let num = display;
     if (num > 0) {
       return (0 - num).toString();
     } else {
